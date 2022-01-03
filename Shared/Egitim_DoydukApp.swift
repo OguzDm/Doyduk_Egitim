4e8df8b86d956e055c5170f58289a246f0955631
Remzi Solmaz (Appcent) <remzi.solmaz@appcent.mobi>
2022-01-03 15:15:45 +0300

    Initial Commit

---
+++ Shared/Egitim_DoydukApp.swift
@@ -0,0 +1,17 @@
+//
+//  Egitim_DoydukApp.swift
+//  Shared
+//
+//  Created by Remzi Solmaz on 3.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Egitim_DoydukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
